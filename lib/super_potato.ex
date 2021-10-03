8e6bde8c1215f850cafe89ea3665736ae78b5f4d
Miguel Benitez <benitezhm@gmail.com>
2021-10-03 21:34:40 +0300

    Added inital functionality to calculate the quantity of fuel required plus tests

--- lib/super_potato.ex
+++ lib/super_potato.ex
@@ -1,18 +1,46 @@
 defmodule SuperPotato do
   @moduledoc """
-  Documentation for `SuperPotato`.
+  This module will let you know how much fuel do you need for trip to space :)
   """
 
+  @typedoc """
+  A tuple representing the planet gravity
+  """
+  @type planet :: {directive :: :launch | :land, gravity :: float()}
+
   @doc """
-  Hello world.
+  Calculate the quantity of fuel required for spaceship trip.
 
   ## Examples
 
-      iex> SuperPotato.hello()
-      :world
+      iex> SuperPotato.calculate_required_fuel(28801, [{:land, 9.807}])
+      13447
 
   """
-  def hello do
-    :world
+  @spec calculate_required_fuel(initial_weight :: integer(), path :: list(planet())) :: integer()
+  def calculate_required_fuel(initial_weight, path) do
+    path
+    |> Enum.reduce(0, fn {_, gravity}, fuel ->
+      initial_fuel =
+        (initial_weight * gravity * 0.033 - 42)
+        |> Kernel.trunc()
+
+      additional_fuel = calculate_additional_fuel(0, initial_fuel, gravity)
+
+      fuel + initial_fuel + additional_fuel
+    end)
+  end
+
+  # caculate the additional fuel required based on the additional weight added by the fuel
+  defp calculate_additional_fuel(additional_fuel, weight, gravity) do
+    (weight * gravity * 0.033 - 42)
+    |> Kernel.trunc()
+    |> case do
+      fuel when fuel > 0 ->
+        calculate_additional_fuel(additional_fuel + fuel, fuel, gravity)
+
+      _fuel ->
+        additional_fuel
+    end
   end
 end
--- test/super_potato_test.exs
+++ test/super_potato_test.exs
@@ -2,7 +2,18 @@ defmodule SuperPotatoTest do
   use ExUnit.Case
   doctest SuperPotato
 
-  test "greets the world" do
-    assert SuperPotato.hello() == :world
+  describe "Test SuperPotato module" do
+    test "fuel required for Apollo 11 to land on earth" do
+      required_fuel =
+        SuperPotato.calculate_required_fuel(28_801, [
+          # {:launch, 9.807},
+          # {:land, 1.62},
+          # {:launch, 1.62},
+          {:land, 9.807}
+        ])
+
+      # to land on earth
+      assert required_fuel == 13_447
+    end
   end
 end
