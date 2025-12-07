af1cf6855e23febf3250a59f4f5a89c16870e6bf
Miguel Benitez <benitezhm@gmail.com>
2025-12-07 15:35:54 +0200

    Refactor version release configuration into separate file

--- config/config.exs
+++ config/config.exs
@@ -7,34 +7,4 @@
 # General application configuration
 import Config
 
-config :version_release,
-  tag_prefix: "v",
-  hex_publish: false,
-  changelog: %{
-    creation: :manual,
-    replacements: [
-      %{
-        file: "CHANGELOG.md",
-        patterns: [
-          %{search: "Unreleased", replace: "{{version}}"},
-          %{search: "...HEAD", replace: "...{{tag_name}}", global: false},
-          %{search: "ReleaseDate", replace: "{{date}}"},
-          %{
-            search: "<!-- next-header -->",
-            replace: "<!-- next-header -->\n\n## [Unreleased] - ReleaseDate",
-            global: false
-          },
-          %{
-            search: "<!-- next-url -->",
-            replace:
-              "<!-- next-url -->\n[Unreleased]: https://github.com/benitezhm/super-potato/compare/{{tag_name}}...HEAD",
-            global: false
-          }
-        ]
-      }
-    ]
-  },
-  merge: [
-    %{from: "master", to: ["develop", "edge"]},
-    %{from: "develop", to: ["edge"]}
-  ]
+import_config "version_release.exs"
---
+++ config/version_release.exs
@@ -0,0 +1,49 @@
+import Config
+
+import Config
+
+replacements = [
+  %{
+    file: "CHANGELOG.md",
+    type: :changelog,
+    patterns: [
+      %{search: "Unreleased", replace: "{{version}}", type: :unreleased},
+      %{search: "...HEAD", replace: "...{{tag_name}}", global: false},
+      %{search: "ReleaseDate", replace: "{{date}}"},
+      %{
+        search: "<!-- next-header -->",
+        replace: "<!-- next-header -->\n\n## [Unreleased] - ReleaseDate",
+        global: false
+      },
+      %{
+        search: "<!-- next-url -->",
+        replace:
+          "<!-- next-url -->\n[Unreleased]: https://github.com/benitezhm/super-potato/compare/{{tag_name}}...HEAD",
+        global: false
+      }
+    ]
+  }
+]
+
+config :version_release,
+  tag_prefix: "v",
+  changelog: %{
+    creation: :manual,
+    minor_patterns: ["added", "changed"],
+    major_patterns: ["breaking"],
+    replacements: replacements,
+    pre_release_replacements: replacements
+  },
+  commit_message: "[skip ci][version_release] {{message}}",
+  dev_version: true,
+  merge: %{
+    ignore_conflicts: true,
+    branches: [
+      %{
+        from: "master",
+        to: ["develop"],
+        strategy: ["recursive", "--strategy-option", "theirs"],
+        message: "[skip ci] Merge branch '{{from}}' into {{to}}"
+      }
+    ]
+  }
